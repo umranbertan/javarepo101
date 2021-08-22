8552093aabfaff1d613ba57db4e713bdc5279c82
umranbertan <umranbertan@gmail.com>
2021-08-22 14:36:40 +0300

    palindrom sayı hesaplandı.

---
+++ returnVoidPractice/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+
+    static int toplam2(int a, int b){
+        return a+b;
+    }
+    static void toplam23(int a, int b){
+        System.out.println( a+b);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
